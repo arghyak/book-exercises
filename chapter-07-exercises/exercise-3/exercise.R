d8f6f16b1ecc86f53e456c59d04a30c52a7966f9
arghyak <arghyak@uw.edu>
2019-07-08 16:13:57 -0700

    completed in class

--- chapter-07-exercises/exercise-3/exercise.R
+++ chapter-07-exercises/exercise-3/exercise.R
@@ -2,12 +2,15 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
+words <- c("apple", "boy", "cat", "dog", "egg", "fish")
+words
 
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (e.g., `'data' is the word of the day!`)
 # Note that the results are more obviously correct with single quotes.
+paste()
 
 
 # Create a vector `a_f_words` which are the elements in `words` that start with 
--- chapter-11-exercises/exercise-5/exercise.R
+++ chapter-11-exercises/exercise-5/exercise.R
@@ -10,22 +10,50 @@ library("dplyr")
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
 
+dep_delay_by_month <- flights %>% 
+  group_by(month) %>% 
+  summarize(delay = mean(dep_delay, na.rm = T))
 
 # Which month had the greatest average departure delay?
 
+which(dep_delay_by_month$delay == max(dep_delay_by_month$delay))
+
+filter(dep_delay_by_month, delay == max(delay)) %>% select(month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay"
 # in that order), you can create a scatterplot by passing that data frame to the
 # `plot()` function
 
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
 
+arr_delay_dest <- flights %>% 
+  group_by(dest) %>% 
+  summarise(delay = mean(arr_delay, na.rm = T)) %>% 
+  arrange(-delay)
+head(arr_delay_dest)
 
 # You can look up these airports in the `airports` data frame!
 
+View(airports)
+colnames(airports)
+
+filter(airports, faa == arr_delay_dest$dest[1])
 
 # Which city was flown to with the highest average speed?
+
+city_highest_speed <- flights %>% 
+  mutate(speed = distance / air_time * 60) %>% 
+  group_by(dest) %>% 
+  summarise(avg_speed = mean(speed, na.rm = T)) %>% 
+  filter(avg_speed == max(avg_speed, na.rm =T))
+
+filter(airports, faa == city_highest_speed$dest[1]) %>% 
+  select(name)
+
+
+
 
